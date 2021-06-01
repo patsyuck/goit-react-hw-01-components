c82d1eb290de2da23342c731c95bbc8d700a7306
Oleksii Patsiuk <patsyuck@ukr.net>
2021-06-01 23:04:33 +0300

    Зроблена заготовка для завдання 1

---
+++ src/Product.js
@@ -0,0 +1,32 @@
+import user from './user.json';
+import './Product.css';
+
+function Product() {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img class="avatar" src={user.avatar} alt="user avatar" />
+        <p class="name">{user.name}</p>
+        <p class="tag">@{user.tag}</p>
+        <p class="location">{user.location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{user.stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{user.stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{user.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Product;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Product from './Product';
+/* import reportWebVitals from './reportWebVitals'; */
 
-ReactDOM.render(
+ReactDOM.render(<Product />, document.getElementById('root'));
+
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); */
