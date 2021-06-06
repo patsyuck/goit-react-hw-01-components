578a08e31aecb9c1cad63d91122bdb7a447bb52e
Oleksii Patsiuk <patsyuck@ukr.net>
2021-06-06 12:34:36 +0300

    Оформлена четверта компонента

--- src/Transactions/Transactions.js
+++ src/Transactions/Transactions.js
@@ -13,16 +13,13 @@ export function Transactions() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Invoice</td>
-          <td>125</td>
-          <td>USD</td>
-        </tr>
-        <tr>
-          <td>Withdrawal</td>
-          <td>85</td>
-          <td>USD</td>
-        </tr>
+        {transactions.map(transaction => (
+          <tr key={transaction.id}>
+            <td>{transaction.type}</td>
+            <td>{transaction.amount}</td>
+            <td>{transaction.currency}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
