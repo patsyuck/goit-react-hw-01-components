6408f4889f6341a070429051680668becc838d04
Oleksii Patsiuk <patsyuck@ukr.net>
2021-06-06 11:46:18 +0300

    Оформлено друге завдання

--- src/Statistics/Statistics.js
+++ src/Statistics/Statistics.js
@@ -7,22 +7,12 @@ export function Statistics() {
     <section class="statistics">
       <h2 class="title">Upload stats</h2>
       <ul class="stat-list">
-        <li class="item">
-          <span class="label">.docx</span>
-          <span class="percentage">4%</span>
-        </li>
-        <li class="item">
-          <span class="label">.mp3</span>
-          <span class="percentage">14%</span>
-        </li>
-        <li class="item">
-          <span class="label">.pdf</span>
-          <span class="percentage">41%</span>
-        </li>
-        <li class="item">
-          <span class="label">.mp4</span>
-          <span class="percentage">12%</span>
-        </li>
+        {statisticalData.map(data => (
+          <li class="item" key={data.id}>
+            <span class="label">{data.label}</span>
+            <span class="percentage">{data.percentage}%</span>
+          </li>
+        ))}
       </ul>
     </section>
   );
